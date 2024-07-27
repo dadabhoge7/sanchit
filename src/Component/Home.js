329182da9cf62b8c8accff7d0689df1376c64f9e
Dada Bhoge <dadabhoge7@gmail.com>
2024-07-27 08:55:36 +0530

    NewProject
    NewProject

---
+++ src/Component/Home.js
@@ -0,0 +1,13 @@
+import Footer from "./Footer";
+import Header from "./Header";
+
+const Home = () => {
+    return(
+        <div>
+            <p>Home</p>
+            <Header></Header>
+            <Footer></Footer>
+        </div>
+    );
+}
+export default Home;
